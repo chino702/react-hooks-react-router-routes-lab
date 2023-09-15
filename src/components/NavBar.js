ed673c3431fd121bc9d105bef069979c4523d65f
chino702 <chinox3702@gmail.com>
2023-09-14 20:09:12 -0700

    Finished with the basic router lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,72 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "block",
+  padding: "6px 0",
+  textDecoration: "none",
+  color: "blue",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            exact
+            style={linkStyles}
+            activeStyle={{
+              fontWeight: "bold",
+              color: "darkblue",
+            }}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/movies"
+            exact
+            style={linkStyles}
+            activeStyle={{
+              fontWeight: "bold",
+              color: "darkblue",
+            }}
+          >
+            Movies
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/directors"
+            exact
+            style={linkStyles}
+            activeStyle={{
+              fontWeight: "bold",
+              color: "darkblue",
+            }}
+          >
+            Directors
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/actors"
+            exact
+            style={linkStyles}
+            activeStyle={{
+              fontWeight: "bold",
+              color: "darkblue",
+            }}
+          >
+            Actors
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
